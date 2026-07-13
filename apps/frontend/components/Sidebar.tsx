0b1cb335a95409f81b2a48f790463c8f1d565199
valvicthe <louie12brett@icloud.com>
2026-07-13 16:28:05 +0100

    Update Sidebar.tsx

--- apps/frontend/components/Sidebar.tsx
+++ apps/frontend/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import Link from 'link';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { 
   Home, 
